8dd18b486e42319976f9c548ba78ace71ca9f7f6
blowle <dydcjf8889@gmail.com>
2021-03-15 14:35:24 +0900

    add rxSwift tutorial code.

--- RxSwiftStudy/ViewController.swift
+++ RxSwiftStudy/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    var shownCities = [String]()
+    let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"]
+    let disposeBag = DisposeBag() // Bag of disposables to release them when view is being deallcated.
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        searchBar
+            .rx.text // Observable property thanks to RxCocoa
+            .orEmpty    // Make it non-optional
+            .debounce(RxTimeInterval.milliseconds(5), scheduler: MainScheduler.instance) // Wait 0.5 for changes.
+            .distinctUntilChanged() // If they didn't occur, check if the new value is the same as old.
+            .filter { !$0.isEmpty }
+            .subscribe(onNext: { [unowned self] query in // Here we will be notified of every new value
+                self.shownCities = self.allCities.filter { $0.hasPrefix(query) || query.hasPrefix("*")} // We now do our "API Request" to find cities.
+                self.tableView.reloadData()
+            })
+            .disposed(by: disposeBag)
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shownCities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cityPrototypeCell", for: indexPath)
+        cell.textLabel?.text = shownCities[indexPath.row]
+        
+        return cell
+    }
+    
+    
 }
 
